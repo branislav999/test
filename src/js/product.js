45ac6d80241bf7bebbfa1ff1e0d8bd0c592cbfad
branislav-bogosavac <branislav-bogosavac@byu.local>
2024-06-07 15:42:09 -0600

    vezba

---
+++ src/js/product.js
@@ -0,0 +1,19 @@
+import { getParam } from "./utils.mjs";
+import { findProductById } from "./productData.mjs";
+import productDetails from "./productDetails.mjs";
+
+
+// // add to cart button event handler
+// async function addToCartHandler(e) {
+//   const product = await findProductById(e.target.dataset.id);
+//   addProductToCart(product);
+// }
+
+// // add listener to Add to Cart button
+// document
+//   .getElementById("addToCart")
+//   .addEventListener("click", addToCartHandler);
+
+
+const productId = getParam('product');
+productDetails(productId)
